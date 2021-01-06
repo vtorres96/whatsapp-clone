b1eec5f24dc10092b8a61efc4e5f766f90c10f8e
vtorres96 <victorcdc96@gmail.com>
2021-01-06 03:17:33 -0300

    added firebase config

---
+++ src/firebaseConfig.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+  apiKey: "",
+  authDomain: "whatsapp-clone-9b582.firebaseapp.com",
+  projectId: "whatsapp-clone-9b582",
+  storageBucket: "whatsapp-clone-9b582.appspot.com",
+  messagingSenderId: "376013459878",
+  appId: "1:376013459878:web:51f490c709efabd1d27f67"
+};
+
+export default firebaseConfig;

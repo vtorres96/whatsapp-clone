0ce3d66bd3866c0093520a6c73b9f1499776ec48
vtorres96 <victorcdc96@gmail.com>
2021-01-06 03:01:32 -0300

    added components styles: Header

---
+++ src/components/Header/styles.js
@@ -0,0 +1,30 @@
+import styled from 'styled-components';
+
+export const Container = styled.div`
+  height: 60px;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;  
+	padding: 0 15px;
+`;
+
+export const Image = styled.img`
+  width: 40px;
+  height: 40px;
+  border-radius: 20px;
+  cursor: pointer;
+`;
+
+export const Buttons = styled.div`
+  display: flex;
+`;
+
+export const Btn = styled.div`
+  width: 40px;
+  height: 40px;
+  border-radius: 20px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  cursor: pointer;
+`;

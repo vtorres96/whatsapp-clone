aaab09dde9b85156eb3a812344ceabb253e1b065
vtorres96 <victorcdc96@gmail.com>
2021-01-06 03:02:23 -0300

    added components styles: SideBar

---
+++ src/components/SideBar/styles.js
@@ -0,0 +1,9 @@
+import styled from 'styled-components';
+
+export const Container = styled.div`
+  width: 35%;
+  max-width: 415px;
+  display: flex;
+  flex-direction: column;
+  border-right: 1px solid #ddd;
+`;

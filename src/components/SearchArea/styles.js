0f0da31be6ed60c4bd1298b6d265d3cd4112215b
vtorres96 <victorcdc96@gmail.com>
2021-01-06 03:05:54 -0300

    added components styles: SearchArea

---
+++ src/components/SearchArea/styles.js
@@ -0,0 +1,24 @@
+import styled from 'styled-components';
+
+export const Container = styled.div`
+  background-color: #f6f6f6;
+  border-bottom: 1px solid #eee;
+	padding: 5px 15px;
+`;
+
+export const SearchInput = styled.div`
+	background-color: #fff;
+	height: 40px;
+	border-radius: 20px;
+	display: flex;
+	align-items: center;
+	padding: 0 10px;
+`;
+
+export const Input = styled.input`
+	flex: 1;
+	border: 0;
+	outline: 0;
+	background-color: transparent;
+	margin-left: 10px;
+`;
